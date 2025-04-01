9eb614aef1d4b344c7b77dd2ea965e2ead9743f9
sumeyye_terzi <sumeyye61terzi@gmail.com>
2025-04-01 23:51:50 +0300

    final sending

---
+++ src/js/02-form.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', e => {
+  console.log(e.target.name);
+  console.log(form.elements);
+  const mail = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+  // console.log(mail);
+  // console.log(message);
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ mail, message })
+  );
+});
+
+let data = localStorage.getItem('feedback-form-state');
+data = JSON.parse(data);
+if (data !== null) {
+  form.elements.email.value = data.mail;
+  form.elements.message.value = data.message;
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(e);
+  localStorage.removeItem('feedback-form-state');
+  e.target.reset();
+});
